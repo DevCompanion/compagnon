c683a5b3a70d7c24e6abb8e2c370194cb3a61e4e
Pixrr <pierry@pixrr.fr>
2023-06-25 19:01:32 +0200

    update inertia config to support external modules

--- app/Helpers/vitePath.php
+++ app/Helpers/vitePath.php
@@ -12,12 +12,31 @@ if (! function_exists('vite_path')) {
     function vite_path(string $component): string
     {
         $parts = explode('::', $component);
-
         if (count($parts) === 1) {
             // This is a normal component, not part of a module.
             return "resources/js/Pages/{$parts[0]}.tsx";
         }
 
-        return "modules/{$parts[0]}/Resources/assets/js/Pages/{$parts[1]}.tsx";
+        if (count($parts) === 2) {
+            $path = "modules/{$parts[0]}/Resources/assets/js/Pages/{$parts[1]}.tsx";
+            if (file_exists(base_path('/') . $path)) {
+                return $path;
+            }
+        }
+
+        if (count($parts) === 3) {
+            $moduleType = match ($parts[0]) {
+                'Modules' => 'modules',
+                'ExternalModules' => 'external-modules',
+                default => throw new \RuntimeException("The component {$component} does not exist."),
+            };
+
+            $path = "{$moduleType}/{$parts[1]}/Resources/assets/js/Pages/{$parts[2]}.tsx";
+            if (file_exists(base_path('/') . $path)) {
+                return $path;
+            }
+        }
+
+        throw new \RuntimeException("The component {$component} does not exist.");
     }
 }
