673b23203080ffc552e491d6d601c667724bc652
Pixrr <pierry@pixrr.fr>
2023-07-23 19:27:28 +0200

    add metadata to uuid generator module

--- modules/UuidGenerator/Http/Controllers/UuidGeneratorController.php
+++ modules/UuidGenerator/Http/Controllers/UuidGeneratorController.php
@@ -7,13 +7,11 @@ namespace Modules\UuidGenerator\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class UuidGeneratorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('UuidGenerator::Index', [
             'uuid' => Str::uuid()->toString(),
