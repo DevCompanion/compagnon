39ec5da06220c70baaf6a1cf50bc6a751de428c0
pixrr <pierry@pixrr.fr>
2023-07-24 21:52:14 +0200

    chore(stubs): update module stubs and documentation

--- modules/UuidGenerator/Http/Controllers/UuidGeneratorController.php
+++ modules/UuidGenerator/Http/Controllers/UuidGeneratorController.php
@@ -8,20 +8,15 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
+use Modules\UuidGenerator\Providers\UuidGeneratorServiceProvider;
+use function sprintf;
 
 class UuidGeneratorController extends Controller
 {
-    private string $module_name;
-
-    public function __construct()
-    {
-        $this->module_name = basename(dirname(__DIR__, 2));
-    }
-
     public function index(): Response
     {
-        return Inertia::render('UuidGenerator::Index', [
-            'ns' => $this->module_name,
+        return Inertia::render(sprintf('%s::Index', UuidGeneratorServiceProvider::$moduleName), [
+            'ns' => UuidGeneratorServiceProvider::$moduleName,
             'uuid' => Str::uuid()->toString(),
         ]);
     }
