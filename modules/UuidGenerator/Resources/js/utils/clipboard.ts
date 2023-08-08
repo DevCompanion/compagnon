92180c23e27ca6e95229cfec11487da7ab3fade6
pixrr <pierry@pixrr.fr>
2023-08-08 23:13:46 +0200

    fix(clipboard): remove the focus on copy

--- modules/UuidGenerator/Resources/js/utils/clipboard.ts
+++ modules/UuidGenerator/Resources/js/utils/clipboard.ts
@@ -2,7 +2,6 @@ const unsecuredCopyToClipboard = (text: string) => {
   const textArea = document.createElement('textarea');
   textArea.value = text;
   document.body.appendChild(textArea);
-  textArea.focus();
   textArea.select();
   try {
     document.execCommand('copy');
